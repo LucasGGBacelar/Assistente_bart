a31e18a6b33dc1e1cbe9194452f54bad55091cba
Lucas_Bacelar_ <111204001+LucasGGBacelar@users.noreply.github.com>
2023-08-08 20:32:49 -0300

    Add files via upload

---
+++ !Readme!.txt
@@ -0,0 +1,39 @@
+!Readme!
+
+
+⁕ Para o funcionamento correto da assistente é preciso esperar ela parar completamente de falar para que ela possa executar o comando
+caso contrário há uma grande possibilidade de dar erro no código;
+
+⁕ Caso você não possua uma chave da api do bard você poderá ir nesse link e fazer o processo para conseguir a sua e tamb
+->  https://github.com/dsdanielpark/Bard-API, após conseguir a chava vá no codigo e altere o "xxxxx" pela sua chave;
+
+⁕ Caso occora um erro de incompreensão do "speech recognition" tente -> 
+
+ -> Verifique as configurações de áudio: Verifique se o seu sistema de áudio está configurado corretamente e se o volume está  
+  ajustado corretamente. Certifique-se de que o áudio não esteja silenciado e que os alto-falantes ou fones de ouvido estejam  
+  conectados corretamente.
+
+ -> Verificar o dispositivo de saída de áudio padrão: Certifique-se de que o dispositivo de saída de áudio padrão esteja corretamente configurado. Você pode verificar e alterar as configurações de áudio do seu sistema operacional para garantir que o áudio seja reproduzido no dispositivo correto.
+
+ -> Verifique se há erros: Verifique se há mensagens de erro ou exceções relacionadas à reprodução de áudio. Certifique-se de que
+ todas as dependências do pyttsx3 estejam instaladas corretamente e atualizadas para a versão mais recente.
+
+
+⁕ O leitor de pdf requer que seja colocado o arquivo pdf na mesma pasta do código.
+
+
+Os comandos são: 
+
+Pergunta -> Você poderá fazer perguntas para a inteligencia artificial Bart que ele irá te responder
+Pausar -> Pausa a mídia que está tocando
+Voltar -> Volta a mídia que está tocando
+Próxima -> Passa para a proxima mídia
+Pesquisa -> Faz uma pesquisa no chrome de acordo com o que você falar
+Jogo ->  Um simples "jogo da cobrinha"
+Leitura -> ultiliza de um leitor de pdf para ler o aquivo pdf de sua preferência (Leia o topico acima!)
+
+
+
+caso aconteça algum erro tente rodar o código novamente, caso não funcione me mande um email!
+
+Email de contato -> lucas.bacelar@yahoo.com
---
+++ main.py
@@ -0,0 +1,200 @@
+import os
+import pyttsx3
+import time
+import speech_recognition as sr
+import pyautogui
+import pygame
+import threading
+from bardapi import Bard
+import random
+import tkinter as tk
+import PyPDF2
+
+r = sr.Recognizer()
+engine = pyttsx3.init()
+limite_sem_audio = 1000
+temporizador = time.time()
+
+done_speaking = threading.Event()
+
+engine.say("O que você deseja fazer?")
+engine.runAndWait()
+
+while True:
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        comando = r.recognize_google(audio, language='pt-BR')
+
+        # Verifica se o comando é uma ação
+        if comando == "pausar":
+            pyautogui.press('playpause')
+            print("Executando ação...")
+            engine.say("Pausando música...")
+            engine.runAndWait()
+
+        elif comando == "voltar":
+            pyautogui.press('prevtrack')
+            print("Voltando música...")
+            engine.say("Voltando música...")
+            engine.runAndWait()
+
+        elif comando == "próxima":
+            pyautogui.press('nexttrack')
+            print("Avançando a música...")
+            engine.say("Avançando música...")
+            engine.runAndWait()
+
+
+        elif comando == "pesquisa":
+            engine.say("Diga o que você quer pesquisar")
+            engine.runAndWait()
+            audio = r.listen(source)
+            palavra = r.recognize_google(audio, language='pt-BR')
+            os.system("start chrome")
+            time.sleep(2)
+            pyautogui.write(palavra)
+            pyautogui.press('enter')
+
+        ######################################################################
+        elif comando == "jogo":
+            engine.runAndWait()
+            pygame.init()
+
+
+            def on_grid_random():
+                x = random.randint(0, 590)
+                y = random.randint(0, 590)
+                return (x // 10 * 10, y // 10 * 10)
+
+
+            def collision(c1, c2):
+                return (c1[0] == c2[0]) and (c1[1] == c2[1])
+
+
+            CIMA = 0
+            DIREITA = 1
+            BAIXO = 2
+            ESQUERDA = 3
+
+            pygame.init()
+            screen = pygame.display.set_mode((500, 500))
+            pygame.display.set_caption('Snake')
+
+            # cobra
+            cobra = [(200, 200), (210, 200), (220, 200)]
+            snake_skin = pygame.Surface((10, 10))
+            snake_skin.fill((0, 255, 0))
+
+            # fruta
+            fruta_pos = on_grid_random()
+            fruta = pygame.Surface((10, 10))
+            fruta.fill((255, 0, 0))
+
+            my_direction = ESQUERDA
+
+            clock = pygame.time.Clock()
+
+            # Controles e saída
+            while True:
+                clock.tick(10)
+                for event in pygame.event.get():
+                    if event.type == QUIT:
+                        pygame.quit()
+
+                    if event.type == KEYDOWN:
+                        if event.key == K_UP:
+                            my_direction = CIMA
+                        if event.key == K_DOWN:
+                            my_direction = BAIXO
+                        if event.key == K_LEFT:
+                            my_direction = ESQUERDA
+                        if event.key == K_RIGHT:
+                            my_direction = DIREITA
+
+                # colisao e aumento de tamanho
+                if collision(cobra[0], fruta_pos):
+                    fruta_pos = on_grid_random()
+                    cobra.append(cobra[-1])
+
+                # Direçoes/Movimentos
+                for i in range(len(cobra) - 1, 0, -1):
+                    cobra[i] = (cobra[i - 1][0], cobra[i - 1][1])
+
+                if my_direction == CIMA:
+                    cobra[0] = (cobra[0][0], cobra[0][1] - 10)
+                if my_direction == BAIXO:
+                    cobra[0] = (cobra[0][0], cobra[0][1] + 10)
+                if my_direction == DIREITA:
+                    cobra[0] = (cobra[0][0] + 10, cobra[0][1])
+                if my_direction == ESQUERDA:
+                    cobra[0] = (cobra[0][0] - 10, cobra[0][1])
+
+                screen.fill((0, 0, 0))
+                screen.blit(fruta, fruta_pos)
+                for pos in cobra:
+                    screen.blit(snake_skin, pos)
+
+                pygame.display.update()
+
+        ##################################################
+        elif comando == "nada":
+            break
+
+        elif time.time() - temporizador > limite_sem_audio:
+            break
+            engine.runAndWait()
+        #######################################################################
+        elif comando == "pergunta":
+
+            os.environ['_BARD_API_KEY'] = "xxxxxx"
+            engine.say("Faça a sua pergunta:")
+            engine.runAndWait()
+
+            # Faz o programa reconhecer seu áudio e transformá-lo em uma pergunta
+            with sr.Microphone() as source:
+                audio = r.listen(source)
+                pergunta = r.recognize_google(audio, language='pt-BR')
+
+                # Escreve e pergunta dita
+                print(pergunta)
+
+                # Usa a biblioteca BardAPI para gerar a resposta
+                resposta = Bard().get_answer(pergunta)['content']
+
+                # Imprime a resposta para o usuário
+                print(resposta)
+
+                # Sintetiza a resposta em voz alta
+                engine.say(resposta)
+                engine.runAndWait()
+        ###########################################################################
+        elif comando == "leitura":
+            speaker = pyttsx3.init()
+            engine.say('Diga o nome do livro')
+            engine.runAndWait()
+
+            with sr.Microphone() as source:
+                audio = r.listen(source)
+            try:
+                nome_arquivo = r.recognize_google(audio, language='pt-BR') + '.pdf'
+            except sr.UnknownValueError:
+                print("Não foi possível reconhecer o áudio. Por favor, tente novamente.")
+                nome_arquivo = None
+
+            if nome_arquivo:
+                livro = open(nome_arquivo, 'rb')
+                pdfReader = PyPDF2.PdfFileReader(livro)
+                paginas = pdfReader.numPages
+                print(paginas)
+                for num in range(0, paginas):
+                    page = pdfReader.getPage(num)
+                    texto = page.extractText()
+                    speaker.say(texto)
+                speaker.runAndWait()
+        else:
+            # Comando não reconhecido
+            print(f"Unrecognized command: {comando}")
+            engine.say("Comando não reconhecido.")
+            engine.runAndWait()
+
+
